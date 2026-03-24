50ce9b5cb077d7143f2a36006add555689829c23
chatts-mv <adam.chatterton@multiverse.io>
2026-03-24 11:39:54 +0000

    Update default application version in main.jsx to V3 and adjust version routing logic

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import App from './App'
 import AppV2 from './v2/AppV2'
 import AppV3 from './v3/AppV3'
 
-// V2 is the default. Navigate to /v1 or /v3 for other versions.
+// V3 is the default. Use /v1 or /v2 for other versions.
 const isV1 = window.location.pathname.startsWith('/v1')
-const isV3 = window.location.pathname.startsWith('/v3')
+const isV2 = window.location.pathname.startsWith('/v2')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {isV1 ? <App /> : isV3 ? <AppV3 /> : <AppV2 />}
+    {isV1 ? <App /> : isV2 ? <AppV2 /> : <AppV3 />}
   </React.StrictMode>,
 )
